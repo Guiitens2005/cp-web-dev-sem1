5b1a67549b772f3502657c401415433d8862d8ae
Guiitens <guilherme.a.caseiro@gmail.com>
2024-09-30 10:20:36 -0300

    Add files via upload

---
+++ CP1-Web-Dev/script.js
@@ -0,0 +1,11 @@
+let variavelUndefined;
+console.log(variavelUndefined);
+
+
+// Declarando uma variável como int
+let numero = 10;
+console.log("Tipo da variável inicialmente:", typeof numero);
+
+// Convertendo a variável para float
+numero = parseFloat(numero);
+console.log("Tipo da variável após a conversão:", typeof numero);
